63e1f5f7263f1d1726a0b610521d9a105fde6462
ZethalMC (Zethal_) <zethalmc@gmail.com>
2024-12-09 15:55:48 +0100

    [WIP] Add tooltips
    Only make sure the convert button doesn't change when disabled and hovered.

--- mc-utilities/book-converter/script.js
+++ mc-utilities/book-converter/script.js
@@ -14,12 +14,15 @@ function showError(message) {
 
 function convertText() {
     const convertButton = document.getElementById("buttonConvert");
+    const tooltip = convertButton.querySelector('.tooltip');
     const size = parseInt(document.getElementById("maxCharSlider").value);
 
     convertButton.disabled = true;
+    tooltip.style.display = 'none';
 
     setTimeout(() => {
         convertButton.disabled = false;
+        tooltip.style.display = 'block';
     }, 3000);
 
     document.getElementById("errorPopup").innerHTML = "";
@@ -94,19 +97,29 @@ function createSaveButtons(books, title, author) {
 
     // Stendhal download button
     var saveButtonStendhal = document.createElement("button");
+    saveButtonStendhal.className = "tt";
     if (books.length > 1) saveButtonStendhal.innerHTML = "Save Stendhal Books";
     else saveButtonStendhal.innerHTML = "Save Stendhal Book";
-    
+
+    saveButtonStendhal.innerHTML += `
+      <span data-text-end="Saved!" data-text-initial="Save Stendhal Books" class="tooltip"></span>
+    `;
+
     saveButtonStendhal.addEventListener("click", (event) => {
         saveBookStendhalFormat(books, title);
     });
-    
+
     buttonContainer.appendChild(saveButtonStendhal);
 
     // Text download button
     var saveButtonTxt = document.createElement("button");
+    saveButtonTxt.className = "tt";
     if (books.length > 1) saveButtonTxt.innerHTML = "Save as .txt Files";
     else saveButtonTxt.innerHTML = "Save as .txt File";
+
+    saveButtonTxt.innerHTML += `
+      <span data-text-end="Saved!" data-text-initial="Save as .txt Files" class="tooltip"></span>
+    `;
 
     saveButtonTxt.addEventListener("click", (event) => {
         saveBookTxtFormat(books, title);
